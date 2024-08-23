de970ef33b5731f044539378dcd1549a0598975d
Julia <musicaparatodo770@gmail.com>
2024-08-23 19:36:19 +0200

    test y finalizo tarea

--- src/test/java/dev/julia/CheckingAccountTest.java
+++ src/test/java/dev/julia/CheckingAccountTest.java
@@ -9,37 +9,37 @@ public class CheckingAccountTest {
 
     @BeforeEach
     public void setUp() {
-        checkingAccount = new CheckingAccount(10000, 1.5f);  // Saldo inicial: 10000, Tasa anual: 1.5%
+        checkingAccount = new CheckingAccount(10000, 1.5f);  
     }
 
     @Test
     public void testWithdrawWithinBalance() {
-        checkingAccount.withdraw(5000);  // Retirar 5000
-        assertEquals(5000, checkingAccount.getBalance(), 0.01);  // El saldo debería ser 5000
-        // No debería haber sobregiro
+        checkingAccount.withdraw(5000);  
+        assertEquals(5000, checkingAccount.getBalance(), 0.01);  
+       
     }
 
     @Test
     public void testWithdrawOverdraft() {
-        checkingAccount.withdraw(15000);  // Retirar 15000, lo que causa un sobregiro
-        assertEquals(0, checkingAccount.getBalance(), 0.01);  // El saldo debería ser 0
-         // El sobregiro debería ser 5000
+        checkingAccount.withdraw(15000);  
+        assertEquals(0, checkingAccount.getBalance(), 0.01);  
+         
     }
 
     @Test
     public void testDepositToCoverOverdraftPartially() {
-        checkingAccount.withdraw(15000);  // Crear un sobregiro de 5000
-        checkingAccount.deposit(3000);  // Consignar 3000
-         // El sobregiro debería reducirse a 2000
-        assertEquals(0, checkingAccount.getBalance(), 0.01);  // No debería haber saldo positivo aún
+        checkingAccount.withdraw(15000);  
+        checkingAccount.deposit(3000);  
+        
+        assertEquals(0, checkingAccount.getBalance(), 0.01); 
     }
 
     @Test
     public void testDepositMoreThanOverdraft() {
-        checkingAccount.withdraw(15000);  // Crear un sobregiro de 5000
-        checkingAccount.deposit(6000);  // Consignar 6000
-         // El sobregiro debería ser 0
-        assertEquals(1000, checkingAccount.getBalance(), 0.01);  // El saldo debería ser 1000
+        checkingAccount.withdraw(15000); 
+        checkingAccount.deposit(6000); 
+        
+        assertEquals(1000, checkingAccount.getBalance(), 0.01);  
     }
 
  
